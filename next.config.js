54b49f62da9ef75c6ec08f45b8184cf6ba6b12ca
Neil Baller <neil@gravybo.at>
2022-12-15 15:36:25 -0600

    Clean up

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: { appDir: true, fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['latin'] } }] },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
 };
 
 module.exports = nextConfig;
