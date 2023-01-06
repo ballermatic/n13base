5f18dbaa7122f8a9e8509e0b4197db834f751b19
Neil Baller <neil@gravybo.at>
2023-01-06 12:00:10 -0600

    Simplify webfont example, tweak styles, use SCSS

--- app/factory/layout.tsx
+++ app/factory/layout.tsx
@@ -1,8 +1,6 @@
-import { headingFont } from '../utils/fonts';
-
 export default function PageLayout({ children }: { children: React.ReactNode }) {
   return (
-    <section className={headingFont.className}>
+    <section>
       {/* Include branch-specific shared UI here */}
       <hr />
       <h5>Layout for page</h5>
