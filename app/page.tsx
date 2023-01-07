b56b9bf9191b396688b73a48e7f2f5be0a6f7403
Neil Baller <neil@gravybo.at>
2023-01-06 18:10:40 -0600

    A bit of styling clean up and a better start page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
+import Link from 'next/link';
 import { SimpleForm } from './components/simple-form';
 
 export default function HomePage() {
   return (
     <div className='container'>
-      <h1 className='exa'>This is n13base</h1>
-      <p>An extremely basic starter using Next.js 13, the app directory, Typescript, and some other goodies.</p>
+      <h1 className='exa' role='heading' aria-level='1'>
+        This is n13base
+      </h1>
+      <p role='doc-subtitle'>An ultra basic Next.js 13 starter.</p>
+      <p>
+        Note the use of ARIA-role to define heading and subheading. Sweet! These can be used{' '}
+        <Link href='https://www.aleksandrhovhannisyan.com/blog/represent-state-with-html-attributes-not-class-names/'>
+          for styling
+        </Link>
+        . While we’re at it, also consider using HTML attributes for{' '}
+        <Link href='https://www.aleksandrhovhannisyan.com/blog/represent-state-with-html-attributes-not-class-names/'>
+          representing state
+        </Link>
+        .{' '}
+      </p>
       <SimpleForm />
     </div>
   );
