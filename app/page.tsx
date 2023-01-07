a4dcd2d2e1f03c7887bc46afacac09aa3ad02954
Neil Baller <neil@gravybo.at>
2023-01-07 08:45:29 -0600

    Fix Aria conflicts with JSX

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,10 @@ import { SimpleForm } from './components/simple-form';
 export default function HomePage() {
   return (
     <div className='container'>
-      <h1 className='exa' role='heading' aria-level='1'>
-        This is n13base
-      </h1>
+      <h1 className='exa'>This is n13base</h1>
       <p role='doc-subtitle'>An ultra basic Next.js 13 starter.</p>
       <p>
-        Note the use of ARIA-role to define heading and subheading. Sweet! These can be used{' '}
+        Note the use of ARIA-role to define subheading. These can be used{' '}
         <Link href='https://www.aleksandrhovhannisyan.com/blog/represent-state-with-html-attributes-not-class-names/'>
           for styling
         </Link>
